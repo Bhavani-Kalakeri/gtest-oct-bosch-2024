44ed2b8e19797965e99469982ee52e7fe921bc42
Bhavani-Kalakeri <BhavaniMadhav.Kalakeri@in.bosch.com>
2024-10-28 16:50:03 +0530

    Update StringCalculatorFixture.Test.cpp

--- StringCalculatorFixture.Test.cpp
+++ StringCalculatorFixture.Test.cpp
@@ -1,1 +1,15 @@
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
+
+Class StringCalculatorAddFixture:public testing::Test
+  protected:
+    StringCalculator *objUnderTest;
+  void Setup() override{
+    objUnderTest = new StringCalculator;
+  }
+void TearDown() override{
+   delete objUnderTest;
+}
+};
+
 
