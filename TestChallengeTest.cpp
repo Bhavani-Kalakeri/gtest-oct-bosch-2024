46ec93a746826c644cc598079edec158baa5440e
Bhavani-Kalakeri <BhavaniMadhav.Kalakeri@in.bosch.com>
2024-10-29 12:13:21 +0530

    Update TestChallengeTest.cpp

--- TestChallengeTest.cpp
+++ TestChallengeTest.cpp
@@ -3,3 +3,13 @@
 
 using namespace std;
 
+TEST(test_add_two_numbers,sum_of_two_numbers){
+  int num1 = 10;
+  int num2 = 7;
+  int* result;
+  int expValue = 17;
+  
+  Add(num1, num2, &result);
+  
+  ASSERT_EQ(expectedValue,result);
+  }
